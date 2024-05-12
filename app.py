98af8f57ce881e7b5308ce5674be6fbbae74a2e9
Wenderson <wenderson_fagundes@hotmail.com>
2024-05-12 17:55:08 -0300

    meu app funcionando

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def hello_world():
+    return "Olá Turma de Dados! Aula de Docker"
+
+
+def main():
+    st.write(hello_world())
+
+
+if __name__ == "__main__":
+    main()
